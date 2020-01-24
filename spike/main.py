148f1881227a7d5f35cc1d1037eb705911d891ff
Nathaniel Flower <nathaniel@rewolf.co>
2020-01-23 20:00:09 -0500

    Refs #2, added projections

--- spike/main.py
+++ spike/main.py
@@ -1,3 +1,26 @@
+import math
+
+
+class Pysics:
+    '''
+    Container class for various physics calculations
+    '''
+
+    @classmethod
+    def degreeprojecttoxy(cls, r: float, theta: float):
+        '''
+        Takes in a radius and theta in degrees
+        Returns corresponding x,y values
+        '''
+        return cls.radianprojecttoxy(r, theta * math.pi / 180)
+
+    @classmethod
+    def radianprojecttoxy(cls, r: float, theta: float):
+        '''
+        Takes in a radius and theta in radians
+        Returns corresponding x,y values
+        '''
+        return r * math.cos(theta), r * math.sin(theta)
 
 
 def main():
