6fcee0d4c1314a3b35f3e34328d1fe31877823c7
Nathaniel Flower <nathaniel@rewolf.co>
2020-01-25 09:24:33 -0500

    Refs #1, moved ui components to its own file

--- spike/main.py
+++ spike/main.py
@@ -1,14 +1,5 @@
 import tkinter as tk
-
-
-class Application(tk.Frame):
-    '''
-    The GUI for the architectual spike application
-    '''
-
-    def __init__(self, master=None):
-        super().__init__(master)
-
+from ui import Application
 
 def main():
     root = tk.Tk()
