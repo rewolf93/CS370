5e37ac2ab3c054b7d72aed5e17d944092e5ffdc0
Nathaniel Flower <nathaniel@rewolf.co>
2020-01-25 19:56:08 -0500

    Refs #17, wrote Physics matrix calculations

--- spike/simulation.py
+++ spike/simulation.py
@@ -1,4 +1,6 @@
-import math
+import numpy as np
+
+CALC_PRECISION = 10
 
 
 class Physics:
@@ -7,17 +9,73 @@ class Physics:
     '''
 
     @classmethod
-    def degreeprojecttoxy(cls, r: float, theta: float):
+    def accelerate(cls, disp_matrix, accel_vec, heading=0):
+        '''
+        Takes a displacement matrix, acceleration vector, and current heading
+        The heading is the angle between the standard axis and the tilted axis of the object
+        Updates the displacement matrix in place
+        '''
+        da = cls.degreeprojectxy(accel_vec, heading=heading)
+        disp_matrix[0][0] = (disp_matrix[1][0] * 2 + da[0])/2
+        disp_matrix[1][0] = (disp_matrix[1][0] * 2 + da[1])/2
+
+    @classmethod
+    def displacement(cls, arry, heading, dt=0.05):
+        '''
+        Calculates the displacement given a matrix of coefficients
+        for the equation s = at^2 + bt^1 + c
+        The matrix must be in the form np.array([(ax, bx, cx), (ay, by, cy)])
+        The new location is stored in the original matrix and given by cx and cy
+        Returns an updated matrix and heading
+        '''
+        timevector = np.array([dt**i for i in range(arry.shape[1]-1, -1, -1)])
+        ds = np.flip(np.sum(arry*timevector, axis=1))
+        dv = np.array([arry[1][0] * dt, arry[0][0] * dt])
+        updates = np.array([(0, 0), dv, ds])
+        updates = np.rot90(updates, k=1, axes=(0, 1))
+        arry += updates
+        arry = np.around(arry, decimals=CALC_PRECISION)
+        vx = arry[0][1]
+        vy = arry[1][1]
+
+        # Figure out new heading via arctan(vy/vx)
+        if vx < 0:
+            if vy < 0:
+                heading = np.arctan(vy/vx) - np.pi
+            else:
+                heading = np.arctan(vy/vx) + np.pi
+        elif vx == 0:
+            if vy == 0:
+                pass
+            elif vy > 0:
+                heading = np.pi/2
+            else:
+                heading = np.pi/-2
+        else:
+            heading = np.arctan(vy/vx)
+        return arry, heading
+
+    @classmethod
+    def degreeprojectxy(cls, vec, heading=0):
         '''
         Takes in a radius and theta in degrees
         Returns corresponding x,y values
         '''
-        return cls.radianprojecttoxy(r, theta * math.pi / 180)
+        vec[1] = (vec[1] / 180) * np.pi
+        return cls.radianprojectxy(vec, heading=heading)
 
     @classmethod
-    def radianprojecttoxy(cls, r: float, theta: float):
+    def radianprojectxy(cls, vec, heading=0):
         '''
         Takes in a radius and theta in radians
         Returns corresponding x,y values
         '''
-        return r * math.cos(theta), r * math.sin(theta)
+        xy = np.array([vec[0] * np.cos(heading+vec[1]),
+                      vec[0] * np.sin(heading+vec[1])])
+        return np.around(xy, decimals=CALC_PRECISION)
+
+    @classmethod
+    def rotateaxis(cls, xy, heading):
+        basis = np.array([(np.cos(heading), -np.sin(heading)),
+                         (np.sin(heading), np.cos(heading))])
+        return np.around(np.sum(xy*basis, axis=1), decimals=CALC_PRECISION)
--- tests/spike/test_simulation.py
+++ tests/spike/test_simulation.py
@@ -1,26 +1,65 @@
-import math
+import numpy as np
 from spike import simulation
 
 PRECISION = 10
 
 
-def test_sphericalprojectionradians():
-    x, y = simulation.Physics.radianprojecttoxy(1.0, 0.0)
+def test_rotateaxis():
+    x, y = simulation.Physics.rotateaxis(np.array([1.0, 0.0]), 0)
     assert round(x, PRECISION) == 1.0 and round(y, PRECISION) == 0.0
 
-    x, y = simulation.Physics.radianprojecttoxy(1.0, math.pi/2.0)
+    x, y = simulation.Physics.rotateaxis(np.array([0.0, 1.0]), 0)
     assert round(x, PRECISION) == 0.0 and round(y, PRECISION) == 1.0
 
-    x, y = simulation.Physics.radianprojecttoxy(2.0, 0.0)
-    assert round(x, PRECISION) == 2.0 and round(y, PRECISION) == 0.0
+    x, y = simulation.Physics.rotateaxis(np.array([1.0, 0.0]), np.pi / 2)
+    assert round(x, PRECISION) == 0.0 and round(y, PRECISION) == 1.0
+
+    x, y = simulation.Physics.rotateaxis(np.array([0.0, 1.0]), np.pi)
+    assert round(x, PRECISION) == 0.0 and round(y, PRECISION) == -1.0
 
 
-def test_sphericalprojectiondegrees():
-    x, y = simulation.Physics.degreeprojecttoxy(1.0, 0.0)
+def test_radianprojectxy():
+    x, y = simulation.Physics.radianprojectxy(np.array([1.0, 0.0]), heading=0)
     assert round(x, PRECISION) == 1.0 and round(y, PRECISION) == 0.0
 
-    x, y = simulation.Physics.degreeprojecttoxy(1.0, 90.0)
+    x, y = simulation.Physics.radianprojectxy(np.array([1.0, np.pi/2.0]),
+                                              heading=0)
     assert round(x, PRECISION) == 0.0 and round(y, PRECISION) == 1.0
 
-    x, y = simulation.Physics.degreeprojecttoxy(2.0, 0.0)
+    x, y = simulation.Physics.radianprojectxy(np.array([2.0, 0.0]), heading=0)
     assert round(x, PRECISION) == 2.0 and round(y, PRECISION) == 0.0
+
+    x, y = simulation.Physics.radianprojectxy(np.array([1.0, 0.0]),
+                                              heading=np.pi/2)
+    assert round(x, PRECISION) == 0.0 and round(y, PRECISION) == 1.0
+
+    x, y = simulation.Physics.radianprojectxy(np.array([1.0, np.pi/2.0]),
+                                              heading=np.pi)
+    assert round(x, PRECISION) == 0.0 and round(y, PRECISION) == -1.0
+
+
+def test_degreeprojectxy():
+    x, y = simulation.Physics.degreeprojectxy(np.array([1.0, 0.0]), heading=0)
+    assert round(x, PRECISION) == 1.0 and round(y, PRECISION) == 0.0
+
+    x, y = simulation.Physics.degreeprojectxy(np.array([1.0, 90.0]), heading=0)
+    assert round(x, PRECISION) == 0.0 and round(y, PRECISION) == 1.0
+
+    x, y = simulation.Physics.degreeprojectxy(np.array([2.0, 0.0]), heading=0)
+    assert round(x, PRECISION) == 2.0 and round(y, PRECISION) == 0.0
+
+    x, y = simulation.Physics.degreeprojectxy(np.array([1.0, 0.0]),
+                                              heading=np.pi/2)
+    assert round(x, PRECISION) == 0.0 and round(y, PRECISION) == 1.0
+
+    x, y = simulation.Physics.degreeprojectxy(np.array([1.0, 90]),
+                                              heading=np.pi)
+    assert round(x, PRECISION) == 0.0 and round(y, PRECISION) == -1.0
+
+
+def test_accelerate():
+    pass
+
+
+def test_displacement():
+    pass
