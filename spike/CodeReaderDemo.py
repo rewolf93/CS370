82c141a291034046f27aeb92a96e294f0fce075f
Ryan-Sciarabba <rhsciarabba@gmail.com>
2020-02-01 13:03:41 -0500

    Added jumping and conditional jumping functionality

--- spike/CodeReaderDemo.py
+++ spike/CodeReaderDemo.py
@@ -1,100 +1,257 @@
-port = ''
-operation = ''
-number = ''
-speed = 0
-angle = 0
+class CodeParser():
 
-def main():
+    #Parsing variables
+    PORT = ''
+    OPERATION = ''
+    NUMBER = ''
+    JUMP = ''
+    FUNC = ''
+    T1 = ''
+    T2 = ''
+    #FUNCTIONS = []
 
-    global port
-    global operation
-    global number
-    
-    #Open racer file
-    racer = open("Racer.txt", "r")
+    path = ''
+    SPEED = 0
+    ANGLE = 0
 
-    #Define symbols
+    lineNum = 0
+
+    #Parsing libraries (or "keywords" if you wanna not sound like an arrogant prick)
     OPERATIONS = ['+', '-', '*', '/', '=']
     NUMBERS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     WHITESPACE = ' '
     PORTS = ["THROTTLE", "TURN"]
-    
-    #Break down symbols together
-    lexeme = ''
-    if racer.mode == "r":
-        code = racer.readlines()
-        for line in code:
-            port = ''
-            operation = ''
-            number = ''
-            lexeme = ''
-            for i, char in enumerate(line):
-                if char != WHITESPACE:
-                   lexeme += char
-                if (i + 1 < len(line)):
-                    if line[i + 1] == WHITESPACE or line[i + 1] == '\n':
-                        if lexeme in PORTS:
-                            port = lexeme
-                        if lexeme in OPERATIONS:
-                            operation = lexeme
-                        if bool([ele for ele in NUMBERS if(ele in lexeme)]):
-                            number = lexeme
-                        lexeme = ''
-                if (i == len(line) - 1):
-                    if lexeme in PORTS:
-                        port = lexeme
-                    if lexeme in OPERATIONS:
-                        operation = lexeme
-                    if bool([ele for ele in NUMBERS if(ele in lexeme)]):
-                        number = lexeme
-                    lexeme = ''
+
+
+    #Initialize the CodeParser
+    def __init__(self, path):
+        self.path = "Racer.txt"
+        self.SPEED = 0
+        self.ANGLE = 0
+
+        #Open racer file
+        self.racer = open(self.path, "r")
+
+        #Read lines in
+        if self.racer.mode == "r":
+            self.code = self.racer.readlines()
+            self.racer.close()
+
+        #Remove whitespace from file
+        line = 0
+        while line < len(self.code):
+            terms = self.code[line].split()
+            if len(terms) == 0:
+                self.code.pop(line)
+            line += 1
                     
-            oprSwitch()
+        self.analyzer()
 
-def addPort():
-    global port
-    print("I'm gonna add", number, "to", port)
-    
-def subPort():
-    global port
-    
-    print("I'm gonna subtract", number, "from", port, "!")
-    
-def mltPort():
-    global port
-    
-    print("I'm gonna multiply", port, "by", number, "!")
-    
-    
-def divPort():
-    global port
 
-    print("I'm gonna divide", port, "by", number, "!")
+    #Analyze
+    def analyzer(self):
 
-def setPort():
-    global port
+        #Keep reading lines until you run out of lines
+        while self.lineNum < len(self.code):
 
-    print("I'm gonna set", port, "to", number, "!")
-    
-def oprSwitch():
+            #Split code line into terms
+            terms = self.code[self.lineNum].split()
 
-    global port
+            #If there are 5 terms go to conditional jumping
+            if len(terms) == 5:
+                self.JUMP = terms[0]
+                self.FUNC = terms[1]
+                self.T1 = terms[2]
+                self.OPERATION = terms[3]
+                self.T2 = terms[4]
 
-    if port == "THROTTLE":
-        port = "speed"
-    if port == "TURN":
-        port = "angle"
-    
-    oprSwitcher = {
-        1: mltPort,
-        2: addPort,
-        4: subPort,
-        6: divPort,
-        20: setPort
-        }
+                print("Conditonally jumping")
+                self.cndJump()
 
-    global operation
-    nOperation = ord(operation) - 41
-    f = oprSwitcher.get(nOperation, "invalid operation")
-    f()
-main()
+            #If there are 3 terms go to math
+            if len(terms) == 3:   
+                self.PORT = terms[0]
+                self.OPERATION = terms[1]
+                self.NUMBER = terms[2]
+                
+                print("Mathing")
+                self.oprSwitch()
+
+            #If there are 2 terms go to jumping
+            if len(terms) == 2:
+                self.JUMP = terms[0]
+                self.FUNC = terms[1]
+                
+                print("Jumping")
+                self.jump()
+
+            #If there is 1 term recognize that it is a function
+            if len(terms) == 1:
+                
+                print("Function")
+                #self.FUNCTIONS.append(terms[0])
+
+            #Print speed and angle after each line read
+            print("Speed:", self.SPEED)
+            print("Angle:", self.ANGLE)
+            self.lineNum += 1
+
+    #Function to add number to port
+    def addPort(self):
+        
+        if self.PORT == "THROTTLE":
+            self.SPEED = self.SPEED + int(self.NUMBER)
+            
+        if self.PORT == "TURN":
+            self.ANGLE = self.ANGLE + int(self.NUMBER)
+
+
+    #Function to subtract number from port 
+    def subPort(self):
+
+        if self.PORT == "THROTTLE":
+            self.SPEED = self.SPEED - self.NUMBER
+            
+        if self.PORT == "TURN":
+            self.ANGLE = self.ANGLE - self.NUMBER
+
+
+    #Function to multply port by number
+    def mltPort(self):
+
+        if self.PORT == "THROTTLE":
+            self.SPEED = self.SPEED * self.NUMBER
+            
+        if self.PORT == "TURN":
+            self.ANGLE = self.ANGLE * self.NUMBER
+
+        
+    #Function to divide port by number    
+    def divPort(self):
+
+        if self.PORT == "THROTTLE":
+            self.SPEED = self.SPEED / self.NUMBER
+            
+        if self.PORT == "TURN":
+            self.ANGLE = self.ANGLE / self.NUMBER
+
+
+    #Function to set port to number
+    def setPort(self):
+
+        if self.PORT == "THROTTLE":
+            self.SPEED = self.NUMBER
+            
+        if self.PORT == "TURN":
+            self.ANGLE = self.NUMBER
+
+
+    #Switch to correct function
+    def oprSwitch(self):
+
+        #Cast variables to ints
+        self.NUMBER = int(self.NUMBER)
+        self.ANGLE = int(self.ANGLE)
+        self.SPEED = int(self.SPEED)
+
+        #Switcherrrrr
+        oprSwitcher = {
+            1: self.mltPort,
+            2: self.addPort,
+            4: self.subPort,
+            6: self.divPort,
+            20: self.setPort
+            }
+
+        #Turn ascii operation into numeral and then go to the switch
+        self.nOperation = ord(self.OPERATION) - 41
+        f = oprSwitcher.get(self.nOperation, "invalid operation")
+        f()
+
+    #Function to jump to spcific line
+    def jump(self):
+
+        #Find the function to jump to and then set lineNum to where it is in the txt file
+        for num, i in enumerate(self.code):
+            terms = self.code[num].split()
+            if self.FUNC == terms[0]:
+                self.lineNum = num
+                return
+
+
+    #Function to jump to specific line conditionally
+    def cndJump(self):
+
+        #Set T1 and T2 to their ports
+        if self.T1 == "THROTTLE":
+            self.T1 = self.SPEED
+        if self.T1 == "TURN":
+            self.T1 = self.ANGLE
+
+        if self.T2 == "THROTTLE":
+            self.T2 = self.SPEED
+        if self.T2 == "TURN":
+            self.T2 = self.ANGLE
+
+        #Make T1 and T2 ints for comparison
+        self.T1 = int(self.T1)
+        self.T2 = int(self.T2)
+
+        #If T1 is less than T2, jump
+        if self.OPERATION == "<":
+            if self.T1 < self.T2:
+                for num, i in enumerate(self.code):
+                    terms = self.code[num].split()
+                    if self.FUNC == terms[0]:
+                        self.lineNum = num
+                        return
+
+        #If T1 is less than or equal to T2, jump    
+        if self.OPERATION == "<=":
+            if self.T1 <= self.T2:
+                for num, i in enumerate(self.code):
+                    terms = self.code[num].split()
+                    if self.FUNC == terms[0]:
+                        self.lineNum = num
+                        return
+
+        #If T1 is greater than T2, jump
+        if self.OPERATION == ">":
+            if self.T1 > self.T2:
+                for num, i in enumerate(self.code):
+                    terms = self.code[num].split()
+                    if self.FUNC == terms[0]:
+                        self.lineNum = num
+                        return
+                    
+        #If T1 is greater than or equal to T2, jump
+        if self.OPERATION == ">=":
+            if self.T1 >= self.T2:
+                for num, i in enumerate(self.code):
+                    terms = self.code[num].split()
+                    if self.FUNC == terms[0]:
+                        self.lineNum = num
+                        return
+
+        #If T1 is equal to T2, jump
+        if self.OPERATION == "=":
+            if self.T1 == self.T2:
+                for num, i in enumerate(self.code):
+                    terms = self.code[num].split()
+                    if self.FUNC == terms[0]:
+                        self.lineNum = num
+                        return
+
+        #If T1 is not equal to T2, jump
+        if self.OPERATION == "!=":
+            if self.T1 != self.T2:
+                for num, i in enumerate(self.code):
+                    terms = self.code[num].split()
+                    if self.FUNC == terms[0]:
+                        self.lineNum = num
+                        return
+        
+
+#This is just for testing and running the code
+green = CodeParser("racer.txt")
+CodeParser.__init__
